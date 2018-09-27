f4b524e1c45c2fe3e6e644d6d9da96203c5cd5f6
Sunny-zz <1348369672@qq.com>
2018-09-27 15:11:26 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,20 +2,7 @@ Page({
   data: {
     text: 'Hello World',
     num: 0,
-    array: [
-      {
-        id: 1,
-        text: 'git'
-      },
-      {
-        id: 2,
-        text: 'git-1'
-      },
-      {
-        id: 3,
-        text: 'git-2'
-      }
-    ],
+
     isRotate: false,
     animation: {}
   },
@@ -43,8 +30,11 @@ Page({
     })
   },
   rotate: function() {
-    const that = this
-    that.animation.rotate(180).scale(1.2).step({ duration: 500 }).rotate(360).scale(1).step({duration:500}).rotate(0).step({duration:0})
-    that.setData({ animation: that.animation.export() })
+    this.animation
+      .rotateY(360)
+      .step({ duration: 1000 })
+      .rotateY(0)
+      .step({ duration: 0 })
+    this.setData({ animation: this.animation.export() })
   }
 })
